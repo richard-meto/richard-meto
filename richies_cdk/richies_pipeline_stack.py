c6bf396c1a5993aef94303c0b134cc271da972a9
Rich Everson <richard.everson@metoffice.gov.uk>
2024-05-30 09:57:57 +0100

    Initial commit

--- richies_cdk/richies_pipeline_stack.py
+++ richies_cdk/richies_pipeline_stack.py
@@ -3,21 +3,21 @@ from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
 
-class RichiesPipelineStack(Stack):
+class RichiesPipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         pipeline = CodePipeline(
             self, "Pipeline",
-            pipeline_name="MyPipeline",
+            pipeline_name="rich-cdk-pipeline",
             synth=ShellStep(
                 "Synth",
                 input=CodePipelineSource.git_hub("richard-meto/richies_cdk", "main"),
                 commands=[
                     "npm install -g aws-cdk",
-                          "python -m pip install -r requirements.txt",
-                          "cdk synth"
+                    "python -m pip install -r requirements.txt",
+                    "cdk synth"
                 ]
             )
         )
